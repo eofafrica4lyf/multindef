11f10a38c37d1f0ab19f6ea62caa2b15c71c5f7d
eofafrica4lyf <aboderinemmanuel@gmail.com>
2019-04-12 10:36:43 -0700

    test to ensure that no parameter is zero

--- multindef.test.js
+++ multindef.test.js
@@ -11,3 +11,7 @@ test('Ensure that a real array is passed',()=>{
 test('Ensure that each parameter of the array is a number',()=>{
     expect(mult([1,2,'e','r','t'])).toBe('....');
 });
+
+test('Ensure that no parameter is equal to zero',()=>{
+    expect(mult([1,2,3,0,9])).toBe(0);
+});
