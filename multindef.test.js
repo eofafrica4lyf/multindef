0dc1ed3ec65422e53e794673aaaa01f567a6da39
eofafrica4lyf <aboderinemmanuel@gmail.com>
2019-04-12 10:08:04 -0700

    test for ascertaining whether parameter is an array

--- multindef.test.js
+++ multindef.test.js
@@ -2,4 +2,8 @@ const mult = require('./multindef');
 
 test('Empty parameter',()=>{
     expect(mult()).toBe('..');
-})
+});
+
+test('Ensure that a real array is passed',()=>{
+    expect(mult({})).toBe('...');
+});
